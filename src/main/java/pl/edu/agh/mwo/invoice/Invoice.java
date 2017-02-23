94ca09d3f3672e35fcc42d239e31a721c38986fb
askorska <agatta@gmail.com>
2017-02-23 01:34:45 +0100

    Invoice complete

--- src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
+++ src/main/java/pl/edu/agh/mwo/invoice/Invoice.java
@@ -2,7 +2,6 @@ package pl.edu.agh.mwo.invoice;
 
 import java.math.BigDecimal;
 import java.util.HashMap;
-import java.util.Map;
 
 import pl.edu.agh.mwo.invoice.product.Product;
 
@@ -10,23 +9,38 @@ public class Invoice {
 	HashMap<Product, Integer> productsMap = new HashMap<Product, Integer>();		
 	
 	public void addProduct(Product product) {
-		// TODO: implement
+		productsMap.put(product,1);
 		
 	}
 
 	public void addProduct(Product product, Integer quantity) {
-		// TODO: implement
+		if (quantity < 1) {
+			throw new IllegalArgumentException();
+		}
+		
+		productsMap.put(product,quantity);
 	}
 
-	public BigDecimal getSubtotal() {
-		return null;
+	public BigDecimal getTotalNet() {
+		BigDecimal totalNet = BigDecimal.ZERO;
+		for (Product key : productsMap.keySet()) {
+		totalNet=totalNet.add((key.getNettoPrice()).multiply(BigDecimal.valueOf(productsMap.get(key))));
+		}
+		return totalNet;
+
 	}
 
 	public BigDecimal getTax() {
-		return null;
+		BigDecimal tax = BigDecimal.ZERO;
+		tax = (this.getTotalGross()).subtract(this.getTotalNet());
+		return tax;
 	}
 
-	public BigDecimal getTotal() {
-		return null;
+	public BigDecimal getTotalGross() {
+		BigDecimal totalGross = BigDecimal.ZERO;
+		for (Product key : productsMap.keySet()) {
+		totalGross=totalGross.add((key.getPriceWithTax()).multiply(BigDecimal.valueOf(productsMap.get(key))));
+		}
+		return totalGross;
 	}
 }
